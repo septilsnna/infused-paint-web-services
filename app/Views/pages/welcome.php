b220ff117f92eb4a9893811d27d9cffa64617476
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-09-29 04:17:41 +0700

    Welcome page

---
+++ app/Views/pages/welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Infused Paint REST API</title>
+</head>
+
+<body style="font-family: Montserrat; background-color: black; color:white">
+    <div class="container">
+        <div class="row my-5">
+            <div class="col">
+                <h2>Welcome to Infused Paint REST API</h2>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
