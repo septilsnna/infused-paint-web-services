dec2f3fdb19ff569bcac0bf682f772490fbcfd23
septilsnna <43660194+septilsnna@users.noreply.github.com>
2020-12-13 23:42:53 +0700

    Sprint controller

---
+++ app/Controllers/Sprint.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Sprint extends BaseController
+{
+    public function index()
+	{
+	    return view('pages/sprint');
+	}
+
+	//--------------------------------------------------------------------
+
+}
